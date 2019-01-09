24a6a6bdb753962ec482e62fe9e94fe64ba84889
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-01-09 20:11:02 +0200

    Added comment.

--- solutions/dcp05.js
+++ solutions/dcp05.js
@@ -1,5 +1,6 @@
 // cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that pair. For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
-// Start: 19:55 End:
+// Start: 19:55 End: 20:10
+// Note: I am pretty sure I do not understand the problem. I tried to recreate the Python code, but having never read or written Python, I am not sure I understand the provided example...
 
 function car(pair) {
   pair.sort((a, b) => a - b);
